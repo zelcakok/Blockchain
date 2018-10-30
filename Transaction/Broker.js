a9e9929ad3c84f23ff301b3a3cf9ed661bb6b3a4
KokTszHoZelca <zelcakok@gmail.com>
2018-10-30 22:39:44 +0800

    Deploy

--- Transaction/Broker.js
+++ Transaction/Broker.js
@@ -67,15 +67,15 @@ class Broker {
 
     this.wallet.transport.addProtocol(PROTOCOLS_NEW_BLK, async (msg)=>{
       var newBlk = msg.message;
-      console.log(newBlk.payload);
-      // var payload = newBlk.payload;
-      // var isTransExist = await this.wallet.db.containsKey("/candidates/"+payload.key);
-      // if(isTransExist){
-        // Log.out("BLK exist in candidates, wipe it.");
+      // console.log(newBlk.payload);
+      var payload = newBlk.payload;
+      var isTransExist = await this.wallet.db.containsKey("/candidates/"+payload.key);
+      if(isTransExist){
+        Log.out("BLK exist in candidates, wipe it.");
         // this.wallet.db.write("/blocks/"+payload.key, newBlk).then(()=>{
           // Log.out("A new block is added to /blocks");
         // })
-      // }
+      }
         // this.propagate(PROTOCOLS_NEW_BLK, "/blocks/"+payload.key, newBlk);
     });
   }
