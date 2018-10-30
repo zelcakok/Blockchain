eb845d829c148fd98630e9c0809b6eba2acfb7e9
KokTszHoZelca <zelcakok@gmail.com>
2018-10-30 23:20:09 +0800

    Deploy

--- Transaction/Broker.js
+++ Transaction/Broker.js
@@ -29,11 +29,7 @@ class Broker {
       var isTransExist = await this.wallet.db.containsKey("/candidates/"+payload.key);
       if(isTransExist) {
         Log.out("WIPE candidates");
-        var old = await this.wallet.db.read("/candidates/"+payload.key,false);
-        console.log("OLD:", old);
-        this.wallet.db.write("/candidates/"+payload.key, "").then(()=>{
-          console.log("/candidates/"+payload.key, "is cleared");
-        });
+        await this.wallet.db.wipe("/candidates/"+payload.key);
       }
     });
     this.wallet.db.monitor("/candidates", async (trans)=>{
