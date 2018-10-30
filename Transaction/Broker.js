7d0f7ab9a719e87d191f131e37a6b998753c393f
KokTszHoZelca <zelcakok@gmail.com>
2018-10-30 19:26:09 +0800

    Deploy

--- Transaction/Broker.js
+++ Transaction/Broker.js
@@ -65,6 +65,7 @@ class Broker {
 
     this.wallet.transport.addProtocol(PROTOCOLS_NEW_BLK, async (msg)=>{
       var newBlk = msg.message;
+      console.log(newBlk.merkleRoot);
       var payload = JSON.parse(newBlk.payload);
       // var isTransExist = await this.wallet.db.containsKey("/candidates/"+payload.key);
       // if(isTransExist)
