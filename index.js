f6d39127f5a4f6b35c28a92cb309ab564c9295d4
KokTszHoZelca <zelcakok@gmail.com>
2018-10-31 16:10:11 +0800

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Wallet = require("./Wallet/Wallet");
 
 async function Main(){
   console.clear();
-  var wallet = await new Wallet("./.zetabase.json", 3049, 3000, 8080, true);
+  var wallet = await new Wallet("./.zetabase.json", 3049, 3000, 8080, false);
   console.log("".padEnd(3)+"Wallet is starting, please wait...");
   setTimeout(function () {
     wallet.startShell();
