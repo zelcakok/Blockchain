3bbb851ade4c3a8852f66f575f1d5644ddd3b647
KokTszHoZelca <zelcakok@gmail.com>
2018-10-31 12:31:28 +0800

    Deploy

--- Database/Zetabase.js
+++ Database/Zetabase.js
@@ -102,9 +102,11 @@ class Zetabase {
       }
       this.prepare().then(()=>{
         var url = this.traverse(path);
+        // console.log(url.dir[url.ptr]);
         // url.dir[url.ptr] = null;
         // delete url.dir[url.ptr];
         // this.eventEmitter.emit('onChanges', path, null);
+        this.eventEmitter.emit('onChanges', path, "WIPE");
         resolve();
       }).catch((err)=>{if(err) {console.log(err); this.wallet.emergency()}});
     });
