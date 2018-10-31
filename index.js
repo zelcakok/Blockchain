d6c3c6f9a68c209377538fe8707e821c60f41ef0
KokTszHoZelca <zelcakok@gmail.com>
2018-10-31 15:34:25 +0800

    Deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Wallet = require("./Wallet/Wallet");
 
 async function Main(){
   console.clear();
-  var wallet = await new Wallet("./.zetabase.json", 3049, 3000, 8080, false);
+  var wallet = await new Wallet("./.zetabase.json", 3049, 3000, 8080, true);
   console.log("".padEnd(3)+"Wallet is starting, please wait...");
   setTimeout(function () {
     wallet.startShell();
@@ -10,14 +10,3 @@ async function Main(){
 }
 
 Main();
-
-// const moment = require("moment");
-// const Cryptographic = require("./Transaction/Cryptographic");
-//
-// var a = moment().valueOf();
-// var b = moment().add(1,'year').valueOf();
-//
-// var ca = Cryptographic.encryptTimestamp(a);
-// var cb = Cryptographic.encryptTimestamp(b);
-//
-// console.log(ca < cb);
