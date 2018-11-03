64762064926c3e3e921f68b2c76b291e9c7caea7
KokTszHoZelca <zelcakok@gmail.com>
2018-11-03 23:58:58 +0800

    Deploy

--- Transaction/Audit.js
+++ Transaction/Audit.js
@@ -3,8 +3,8 @@ module.exports = (blocks)=>{
     Object.keys(blocks).map((transKey)=>{
       var block = JSON.parse(blocks[transKey]);
       if(block.hasOwnProperty("payload")) {
-        var payment = block.payload.payment;
-        console.log(payment, block);
+        var payment = JSON.parse(block.payload).payment;
+        console.log(payment);
         if(!ledger.includes(payment.tarAddr)) ledger[payment.tarAddr] = 0;
         ledger[payment.tarAddr] += payment.amount;
       }
