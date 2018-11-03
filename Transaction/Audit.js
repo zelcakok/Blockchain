eae417b794d01d7b148c45e03b52cf6d40741cad
KokTszHoZelca <zelcakok@gmail.com>
2018-11-03 23:55:18 +0800

    Deploy

--- Transaction/Audit.js
+++ Transaction/Audit.js
@@ -1,11 +1,12 @@
 module.exports = (blocks)=>{
     var ledger = [];
     Object.keys(blocks).map((transKey)=>{
-      var transaction = JSON.parse(blocks[transKey]);
-      console.log("TRANS", transaction);
-      // var payment = blocks[transKey].payload.payment;
-      // if(!ledger.includes(payment.tarAddr)) ledger[payment.tarAddr] = 0;
-      // ledger[payment.tarAddr] += payment.amount;
+      var block = JSON.parse(blocks[transKey]);
+      if(tranaction.hasOwnProperty("payload")) {
+        var payment = block.payload.payment;
+        if(!ledger.includes(payment.tarAddr)) ledger[payment.tarAddr] = 0;
+        ledger[payment.tarAddr] += payment.amount;
+      }
     })
     return Promise.resolve(ledger);
 };
